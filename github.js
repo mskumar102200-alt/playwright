a9a0bcbcdee046c713405d4293898999ee5e7b38
mskumar102200 <mskumar102200@gmail.com>
2026-05-06 13:03:40 +0530

    This is my first commit

---
+++ github.js
@@ -0,0 +1,50 @@
+/*
+what is Git?
+Git is a version control system
+save different versions of your code
+go back to pervious version if something break
+git helps you keep track of code changes
+git is used to collabration on code
+git and github are different
+git - tool
+
+features of git 
+when a files is change, added or deleted it is considered as modify
+you select the modified files you want to stage
+the stage files are committed which promts git to store a permanent snapshot of the files
+git always you to see full files of history of every commands
+you can revert back to pervious any commit
+*/
+
+/*
+what is github
+it is online platform
+git is not the some as github
+github makes tools that use git
+git is the largest host of source code in the world and as has been owned by microsoft
+
+*/
+
+/*hwt git is used in playwright testing
+storing testscript
+team collabration
+ci/cd intergration / continue depolyment / continue integration
+version control(buck handling)
+branching for features
+backup and safety
+*/
+
+let a = "Selvakumar"
+console.log(a)
+
+//git init - used to create new repository
+//pwd - print working directory
+//git version - 2.42.0.windows.2
+//ls - list file(-)and directory(d)
+//cd - change directory
+//cat - display file content
+//clone - copies from a repository from removed source
+//cp - copy file or repository
+// git statu - shows the curent of working directory
+//git add - stages changes for commit
+//git commit - save changes with a message
